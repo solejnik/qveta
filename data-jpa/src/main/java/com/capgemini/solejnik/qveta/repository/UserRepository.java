a19776db123e9a7b884eadf259b5009198a1edaf
11cervantes11 <SOLEJNIK@CE09507.corp.capgemini.com>
2016-04-04 15:47:34 +0200

    imported conxext from data-jpa to web module

--- data-jpa/src/main/java/com/capgemini/solejnik/qveta/repository/UserRepository.java
+++ data-jpa/src/main/java/com/capgemini/solejnik/qveta/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.capgemini.solejnik.qveta.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import com.capgemini.solejnik.qveta.entity.UserEntity;
 
-@Service
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
 }
--- web/src/com/capgemini/solejnik/qveta/controller/SampleController.java
+++ web/src/com/capgemini/solejnik/qveta/controller/SampleController.java
@@ -5,17 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.capgemini.solejnik.qveta.repository.UserRepository;
 import com.capgemini.solejnik.qveta.service.UserService;
 
 @Controller
 public class SampleController {
 	@Autowired
-	private UserService service;
+	private UserRepository repo;
 
 	@RequestMapping("/sampleVisit")
 	public ModelAndView helloWorld() {
-		Long message = service.getFirst().getId();
-		return new ModelAndView("sampleVisit", "message", message);
+//		Long message = service.getFirst().getId();
+		return new ModelAndView("sampleVisit", "message", 1l);
 	}
 
 }
