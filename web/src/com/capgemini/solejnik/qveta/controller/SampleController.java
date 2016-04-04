5078937b9f5b64a94466e1c2b875e7d478556d2f
11cervantes11 <SOLEJNIK@CE09507.corp.capgemini.com>
2016-04-04 16:05:14 +0200

    working sampleVisit.jsp

--- web/src/com/capgemini/solejnik/qveta/controller/SampleController.java
+++ web/src/com/capgemini/solejnik/qveta/controller/SampleController.java
@@ -5,18 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.capgemini.solejnik.qveta.enums.RoleEnum;
 import com.capgemini.solejnik.qveta.repository.UserRepository;
 import com.capgemini.solejnik.qveta.service.UserService;
+import com.capgemini.solejnik.qveta.to.UserTo;
 
 @Controller
 public class SampleController {
 	@Autowired
-	private UserRepository repo;
+	private UserService service;
 
 	@RequestMapping("/sampleVisit")
 	public ModelAndView helloWorld() {
-//		Long message = service.getFirst().getId();
-		return new ModelAndView("sampleVisit", "message", 1l);
+		UserTo userTo = new UserTo("Simon", "Olejnik", "simon@olejnik.com", "123", RoleEnum.ADMIN);
+		service.saveUser(userTo);
+		return new ModelAndView("sampleVisit", "message", userTo);
 	}
 
 }
